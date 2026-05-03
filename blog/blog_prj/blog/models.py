03853ce00768f62ca14e393f7e5bf6a4f671a768
Yoo-Su-Hyeon <jjongari01@naver.com>
2026-05-03 18:27:09 +0900

    [0503]유수현 워크북 과제

--- blog/blog_prj/blog/models.py
+++ blog/blog_prj/blog/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from users.models import User
+import os
+from uuid import uuid4
+from django.utils import timezone
+
+def upload_filepath(instance, filename):
+    today_str = timezone.now().strftime("%Y%m%d")
+    file_basename = os.path.basename(filename)
+    return f'{instance._meta.model_name}/{today_str}/{str(uuid4())}_{file_basename}'
 
 # Create your models here.
 class Post(models.Model):
@@ -7,6 +15,8 @@ class Post(models.Model):
     content=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="posts")
+    image = models.ImageField(upload_to=upload_filepath, blank = True)
+    video = models.FileField(upload_to=upload_filepath, blank = True)
     
     def __str__(self):
         return f'[{self.id}] self.title'
@@ -19,3 +29,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'[{self.id}] {self.content}'
+
